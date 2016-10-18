789a088208f521865f2c1b9913e345f149c0f2c6
Javier Mera <javier_mera88@hotmail.com>
2016-10-18 11:16:21 -0500

    Refactored each view's attributes to their own styles, and made generic styles for views that were using the same values for some attributes. Now the layout xml looks somewhat cleaner :D

--- app/src/main/java/movienight/javi/com/movienight/ui/SearchActivity.java
+++ app/src/main/java/movienight/javi/com/movienight/ui/SearchActivity.java
@@ -1,14 +1,11 @@
 package movienight.javi.com.movienight.ui;
 
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.LayerDrawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.InputDevice;
 import android.widget.RatingBar;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +22,6 @@ public class SearchActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        ((LayerDrawable)mRatingBar.getProgressDrawable()).getDrawable(2).setColorFilter(ContextCompat.getColor(this, R.color.movie_title_text_color), PorterDuff.Mode.SRC_ATOP);
+        ((LayerDrawable)mRatingBar.getProgressDrawable()).getDrawable(2).setColorFilter(ContextCompat.getColor(this, R.color.movie_dark_purple), PorterDuff.Mode.SRC_ATOP);
     }
 }
