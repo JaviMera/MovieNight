5f9c7a26f7030e2ca4b3337fdcd2fa2df2f3e337
Javier Mera <javier_mera88@hotmail.com>
2016-10-18 23:54:00 -0500

    Changed activity search layout to use a seek bar instead of a rating bar. The rating bar was giving me issues with indentation inside of the table row, and the stars would not align correctly (no pwn intended). Moved the date icon to be a drawable resource instead of a mimap.

--- app/src/main/java/movienight/javi/com/movienight/ui/SearchActivity/SearchActivity.java
+++ app/src/main/java/movienight/javi/com/movienight/ui/SearchActivity/SearchActivity.java
@@ -3,32 +3,21 @@ package movienight.javi.com.movienight.ui.SearchActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.LayerDrawable;
-import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.OnFocusChange;
 import movienight.javi.com.movienight.R;
 import movienight.javi.com.movienight.adapters.GenreSpinnerAdapter;
 import movienight.javi.com.movienight.model.Genre;
@@ -39,9 +28,10 @@ public class SearchActivity extends AppCompatActivity implements SearchActivityV
     private SearchActivityPresenter mPresenter;
     private DatePickerFragmentDialog mDialog;
 
-    @BindView(R.id.ratingBarView) RatingBar mRatingBar;
+    @BindView(R.id.seekBarView) SeekBar mSeekBarView;
     @BindView(R.id.genreSpinnerView) Spinner mGenreSpinner;
-    @BindView(R.id.datePickerEditTextView) EditText mReleaseDateEditTextView;
+    @BindView(R.id.datePickerButtonView)
+    AppCompatButton mReleaseDateEditTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,7 +61,7 @@ public class SearchActivity extends AppCompatActivity implements SearchActivityV
         mGenreSpinner.setAdapter(adapter);
     }
 
-    @OnClick(R.id.datepickerImageView)
+    @OnClick(R.id.datePickerButtonView)
     public void onDatepickerImageClick(View view) {
 
         mDialog.show(getSupportFragmentManager(), "dialog_tag");
