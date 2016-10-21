b2911c2848ec2e8e704c99e3c34247d328607e61
Javier Mera <javier_mera88@hotmail.com>
2016-10-21 09:57:04 -0500

    Created package and class for moves activity

---
+++ app/src/main/java/movienight/javi/com/movienight/ui/MoviesActivity/MoviesActivity.java
@@ -0,0 +1,15 @@
+package movienight.javi.com.movienight.ui.MoviesActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import movienight.javi.com.movienight.R;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
