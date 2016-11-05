34f08d75043eb53d9d15ba92f508a71a3ce9cf26
Javier Mera <javier_mera88@hotmail.com>
2016-11-05 14:31:32 -0500

    Fixed tv show url where now it will request the correct url with the applied filters. Before it was sending always the same default url

--- app/src/main/java/movienight/javi/com/movienight/urls/TVShowUrl.java
+++ app/src/main/java/movienight/javi/com/movienight/urls/TVShowUrl.java
@@ -9,8 +9,13 @@ public class TVShowUrl extends AbstractUrl {
     private final String mUrl = "https://api.themoviedb.org/3/discover/tv?api_key=";
     private final String language = "language";
     private final String sort = "sort_by";
+    private final String minAirDate = "air_date.gte";
+    private final String maxAirDate = "air_date.lte";
     private final String page = "page";
     private final String timezone = "timezone";
+    private final String vote = "vote_count.gte";
+    private final String rating = "vote_average.gte";
+    private final String genres = "with_genres";
     private final String includeNullFirstAirDates = "include_null_first_air_dates";
 
     private String mLanguage;
@@ -65,8 +70,35 @@ public class TVShowUrl extends AbstractUrl {
         request += mApiKeyParameter;
         request += "&" + language + "=" + mLanguage;
         request += "&" + sort + "=" + mSort;
+
+        if(!mStartDate.isEmpty()) {
+
+            request += "&" + minAirDate + "=" + mStartDate;
+        }
+
+        if(!mEndDate.isEmpty()) {
+
+            request += "&" + maxAirDate + "=" + mEndDate;
+        }
+
         request += "&" + page + "=" + mPage;
         request += "&" + timezone + "=" + mTimezone;
+
+        if(!mRating.isEmpty()) {
+
+            request += "&" + rating + "=" + mRating;
+        }
+
+        if(!mVoteCount.isEmpty()) {
+
+            request += "&" + vote + "=" + mVoteCount;
+        }
+
+        if(!mGenres.isEmpty()) {
+
+            request += "&" + genres + "=" + mGenres;
+        }
+
         request += "&" + includeNullFirstAirDates + "=" + mFirstAirDates;
 
         return request;
